21398f84c9adfa5f379e7fa93dcae4eccf75356d
Spoon Seah <liwei@spoon.com.sg>
2022-11-23 18:21:31 +0800

    changed FRx Download Icon

--- src/resources/theme/Images.js
+++ src/resources/theme/Images.js
@@ -2,7 +2,7 @@ export const IMAGES = {
   BUY: require("../../assets/images/buy.png"),
   DOWN_ARROW: require("../../assets/images/down-arrow.png"),
   EARN: require("../../assets/images/earn.png"),
-  DOWNLOAD: require("../../assets/images/frx-download-icons.png"),
+  DOWNLOAD: require("../../assets/images/frx-download-icons.svg"),
   LOGO: require("../../assets/images/frx-logo.png"),
   PARK: require("../../assets/images/park.png"),
   QRCODE: require("../../assets/images/qrcode.png"),
